8d20d6dc189021cc5a68cd1d1eba62ebefb7a332
onekill-kr <kimdonghyeok0529@gmail.com>
2023-03-21 15:26:06 +0900

    new choice.py rockscissorspaper.py
    update
    random.py

---
+++ 20230321/choice.py
@@ -0,0 +1,3 @@
+choice = int(input('만날까? 말까? (0/1) 입력 : '))
+print('오늘부터 1일\n') if choice == 1 else print('다음 기회에~~\n')
+
--- 20230321/random.py
+++ 20230321/random.py
@@ -2,5 +2,7 @@ import random
 trip = random.randrange(0, 4)
 if trip == 0 :
     print('Jeju island')
-else if trip == 1 :
+elif trip == 1 :
     print('Hawai island')
+else :
+    print('Dokdo island')
